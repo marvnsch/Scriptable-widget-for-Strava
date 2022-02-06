36179349d608a68e19008233e44cf2de22b6b537
Marvin <marvnsch@mail.uni-paderborn.de>
2022-02-06 22:32:05 +0100

    improved darkmode/lightmode

--- website/script.js
+++ website/script.js
@@ -12,6 +12,7 @@ bd.addEventListener('mousemove', e => {
 });
 
 const textClr = document.querySelectorAll('.mode-switch');
+const apiLogo = document.querySelector('#api-logo');
 const switchPoint = document.querySelector('.darkmode-switch');
 const modeSwitch = document.querySelector('.darkmode-switch-frame');
 let darkModeEnabled = true
@@ -22,6 +23,7 @@ modeSwitch.addEventListener('click', event => {
             element.style.color = 'rgb(59, 58, 62)';
             });
         bd.style.backgroundColor = 'rgb(255, 255, 255)';
+        apiLogo.src = 'api_logo_grey.png'
         switchPoint.style.left = '48px'
         darkModeEnabled = false;
     } else {
@@ -29,6 +31,7 @@ modeSwitch.addEventListener('click', event => {
             element.style.color = 'rgb(255, 255, 255)';
         });
         bd.style.backgroundColor = 'rgb(29, 28, 33)';
+        apiLogo.src = 'api_logo_white.png'
         switchPoint.style.left = '3px'
         darkModeEnabled = true;
     }
